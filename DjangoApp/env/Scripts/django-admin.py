be6326e44f887bbbb8f7737286a4c3b417a7a7a7
Jame Show Leiong Hii <Jame.HiiShowLeiong@wdc.com>
2019-03-29 09:35:18 +0800

    Django app tutorial part 2

---
+++ DjangoApp/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\1000108108\Desktop\Learning\VisualStudioCode\DjangoApp\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
