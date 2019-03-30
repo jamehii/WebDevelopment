89f13dd51f7cf37c78a0540e1c9e3f14c523844e
Jame Show Leiong Hii <Jame.HiiShowLeiong@wdc.com>
2019-03-30 09:32:49 +0800

    Django tutorial part 5: Improving our view
    Question.objects.filter(pub_date__lte=timezone.now())
    - returns a queryset containing Questions
      whose pub_date is less than or equal to - that is,
      earlier than or equal to - timezone.now.

--- DjangoApp/mysite/polls/views.py
+++ DjangoApp/mysite/polls/views.py
@@ -8,6 +8,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.template import loader
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -26,9 +27,12 @@ class IndexView(generic.ListView):
 
     # override this function to return "list of data" back to context_object_name = 'latest_question_list'
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
 
+        return Question.objects.filter(pub_date__lte = timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
  
