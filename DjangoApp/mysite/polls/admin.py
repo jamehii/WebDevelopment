2b78d34bb12accf2a28c7625275a2792642cdd96
Jame Show Leiong Hii <Jame.HiiShowLeiong@wdc.com>
2019-03-30 16:46:20 +0800

    Django tutorial part 7: Customize admin using attribute: fieldsets
    class QuestionAdmin(admin.ModelAdmin):
        fieldsets = [
            (None,               {'fields': ['question_text']}),
            ('Date information', {'fields': ['pub_date']}),
        ]

--- DjangoApp/mysite/polls/admin.py
+++ DjangoApp/mysite/polls/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 from .models import Question
 
+
 # Register your models here.
+
+"""
+Admin Customize 1
+=====================
+
 class QuestionAdmin(admin.ModelAdmin):
     fields = ['pub_date', 'question_text']
+"""
 
+"""
+Admin Customize 2 ( fieldsets )
+=====================
+"""
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               { 'fields' : ['question_text']}),
+        ('Date Information', { 'fields' : ['pub_date']}),
+
+    ]
 
 admin.site.register(Question, QuestionAdmin)
