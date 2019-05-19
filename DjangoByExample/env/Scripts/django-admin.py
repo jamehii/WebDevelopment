fdb2da3602879bf819e6ed9e199c8f7ddc6831a0
Jame Show Leiong Hii <Jame.HiiShowLeiong@wdc.com>
2019-05-19 10:06:50 +0800

    Add html/css Setup new django project for practice

---
+++ DjangoByExample/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\1000108108\desktop\learning\webdevelopment\djangobyexample\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
